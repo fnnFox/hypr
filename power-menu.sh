27ce71370520e81e44af0eef79c3adb215d5329e
Will Corvo <nick.neytron@yandex.ru>
2026-02-22 16:57:27 +0300

    change lock binds; power-menu script instead of wlogout

---
+++ power-menu.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+choice=$(printf "Lock\nSuspend\nLogout\nReboot\nPoweroff" | rofi -dmenu -p "System control")
+
+case "$choice" in
+	Lock) loginctl lock-session ;;
+	Suspend) systemctl suspend ;;
+	Logout) hyprctl logout ;;
+	Reboot) systemctl reboot ;;
+	Poweroff) systemctl poweroff ;;
+esac
