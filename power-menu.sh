ec92dfba6d44c970333a15508f0b98e650861608
Will Corvo <nick.neytron@yandex.ru>
2026-05-20 08:32:20 +0300

    change Logout command

--- power-menu.sh
+++ power-menu.sh
@@ -5,7 +5,7 @@ choice=$(printf "Lock\nSuspend\nLogout\nReboot\nPoweroff" | rofi -dmenu -case-sm
 case "$choice" in
 	Lock) hyprlock ;;
 	Suspend) systemctl suspend ;;
-	Logout) hyprctl logout ;;
+	Logout) hyprctl dispatch 'hl.dsp.exit()' ;;
 	Reboot) systemctl reboot ;;
 	Poweroff) systemctl poweroff ;;
 esac
